cc7076d69e5d96892ea8502d8f595744c6cfd922
BuiQuangMinh30 <buiquangminh30102001@gmail.com>
2023-07-03 12:01:40 +0700

    fix noti warning

--- App.js
+++ App.js
@@ -1,6 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { registerRootComponent } from "expo";
+import './ignoreWarning';
 import { Provider, useDispatch, useSelector } from "react-redux";
 import "react-native-gesture-handler";
 import AppNav from "./src/navigation/AppNav";
@@ -8,14 +6,11 @@ import { store } from "./src/redux/store";
 import { LogBox } from "react-native";
 import moment from "moment";
 require('moment/locale/vi');
-import { YellowBox } from 'react-native';
 import { useEffect } from "react";
 
 
 export default function App() {
-  useEffect(() => {
-    YellowBox.ignoreWarnings(['VirtualizedLists should never be nested']);
-}, [])
+
 
   return (
     <>
---
+++ firebase.js
@@ -0,0 +1,29 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeApp } from 'firebase/app';
+import {
+  initializeAuth,
+  getReactNativePersistence
+} from 'firebase/auth/react-native';
+
+// Replace this with your Firebase SDK config snippet
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDbtJM8-eC0iE4mLH6hPRl4G8_AijNq0Q8",
+    authDomain: "reactnative-mess.firebaseapp.com",
+    databaseURL: "https://reactnative-mess-default-rtdb.firebaseio.com",
+    projectId: "reactnative-mess",
+    storageBucket: "reactnative-mess.appspot.com",
+    messagingSenderId: "110129240809",
+    appId: "1:110129240809:web:7b09c3731a79c54ef66e78",
+    measurementId: "G-LB50JG95TP"
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Auth
+const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage)
+});
+
+export {auth};
