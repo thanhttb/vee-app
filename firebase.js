9100b020c78dd56d05fd9e038a2216cc59755f19
BuiQuangMinh30 <buiquangminh30102001@gmail.com>
2023-08-04 16:00:54 +0700

    config env firebase

--- firebase.js
+++ firebase.js
@@ -1,27 +1,24 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
-
-console.log(" process.env.REACT_APP_APIKEY", process.env.REACT_APP_APIKEY);
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+} from "@env";
 
 // Replace this with your Firebase SDK config snippet
 const firebaseConfig = {
   /* YOUR FIREBASE CONFIG OBJECT PROPERTIES HERE */
-  // apiKey: process.env.REACT_APP_APIKEY,
-  // authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  // databaseURL: process.env.REACT_APP_DATABASEURL,
-  // projectId: process.env.REACT_APP_PROJECTID,
-  // storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  // messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  // appId: process.env.REACT_APP_APPID,
-  // measurementId: process.env.REACT_APP_MEASUREMENTID
-
-  apiKey: "AIzaSyDmlLl9XoEQ05MaGOdmJ-0SL6N8-QQhCTc",
-  authDomain: "vietelite-chat.firebaseapp.com",
-  projectId: "vietelite-chat",
-  storageBucket: "vietelite-chat.appspot.com",
-  messagingSenderId: "578384786335",
-  appId: "1:578384786335:web:4f2fd313a3d1fb608943aa",
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
 };
 
 // Initialize Firebase
